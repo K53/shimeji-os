c88535d0abb7c84d638c601eff7da4aacc634d43
keigo <devenv.keigo@gmail.com>
2022-07-10 02:11:22 +0900

    feat: read file

--- Makefile
+++ Makefile
@@ -1,33 +1,33 @@
 BOOTLOADER_PATH=./bootloader/target/x86_64-unknown-uefi/debug/bootloader.efi 
-# KERNEL_PATH=./kernel/target/x86_64-unknown-none-shimejios/debug/kernel.elf 
+KERNEL_PATH=./kernel/target/x86_64-unknown-none-shimejios/debug/kernel.elf 
 DISK_IMG_PATH=./disk.img
 MOUNT_POINT=./mnt
 
 .PHONY: run
 run: $(DISK_IMG_PATH)
-	qemu-system-x86_64 -drive if=pflash,file=OVMF_CODE.fd -drive if=pflash,file=OVMF_VARS.fd -hda $(DISK_IMG_PATH)
+	qemu-system-x86_64 -drive if=pflash,file=OVMF_CODE.fd -drive if=pflash,file=OVMF_VARS.fd -hda $(DISK_IMG_PATH) -monitor stdio
 
 
-$(DISK_IMG_PATH): $(BOOTLOADER_PATH) #$(KERNEL_PATH)
+$(DISK_IMG_PATH): $(BOOTLOADER_PATH) $(KERNEL_PATH)
 	qemu-img create -f raw $(DISK_IMG_PATH) 200M
 	mkfs.fat -n 'ShimejiOS' -s 2 -f 2 -R 32 -F 32 $(DISK_IMG_PATH)
 	hdiutil attach -mountpoint $(MOUNT_POINT) $(DISK_IMG_PATH)
 	sleep 0.5
 	mkdir -p $(MOUNT_POINT)/EFI/BOOT
 	cp $(BOOTLOADER_PATH) $(MOUNT_POINT)/EFI/BOOT/BOOTX64.EFI
-#	cp $(KERNEL_PATH) $(MOUNT_POINT)/kernel.elf
-	sleep 0.5
+	cp $(KERNEL_PATH) $(MOUNT_POINT)/kernel.elf
+	sleep 1
 	hdiutil detach $(MOUNT_POINT)
 
 
 $(BOOTLOADER_PATH): ./bootloader/src/*.rs ./bootloader/Cargo.toml ./bootloader/.cargo/*
 	docker run -v $(shell pwd):/rust/ shimeji /bin/bash -c "cd /rust/bootloader && cargo +nightly-2022-06-18 rustc --target x86_64-unknown-uefi -Z build-std"
 
-# $(KERNEL_PATH): ./kernel/src/*.rs ./kernel/Cargo.toml ./kernel/.cargo/* ./kernel/x86_64-unknown-none-mikanos-rust.json
-# 	docker run -v $(shell pwd):/rust/ rust-uefi /bin/bash -c "cd /rust/kernel && cargo build"
+$(KERNEL_PATH): ./kernel/src/*.rs ./kernel/Cargo.toml ./kernel/.cargo/* ./kernel/x86_64-unknown-none-shimejios.json
+	docker run -v $(shell pwd):/rust/ rust-uefi /bin/bash -c "cd /rust/kernel && cargo build"
 
 .PHONY: clean
 clean:
 	-rm -f $(DISK_IMG_PATH)
 	-rm -f $(BOOTLOADER_PATH)
-#	-rm -f $(KERNEL_PATH)
+	-rm -f $(KERNEL_PATH)
--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -5,11 +5,16 @@
 #[macro_use]
 extern crate alloc;
 
+
 use core::arch::asm;
 use alloc::vec::Vec;
 use core::fmt::Write;
 use uefi::prelude::*;
+use uefi::{CStr16, Handle};
 use uefi::table::runtime::ResetType;
+use uefi::proto::loaded_image::LoadedImage;
+use uefi::proto::media::fs::SimpleFileSystem;
+use uefi::proto::media::file::{File, RegularFile, Directory, FileMode, FileAttribute};
 
 #[entry]
 fn efi_main(_image: Handle, mut system_table: SystemTable<Boot>) -> Status {
@@ -22,9 +27,32 @@ fn efi_main(_image: Handle, mut system_table: SystemTable<Boot>) -> Status {
     let revision = system_table.uefi_revision();
     let (major, minor) = (revision.major(), revision.minor());
     log::info!("UEFI {}.{}", major, minor);
-    
+
     let boot_services = system_table.boot_services();
     get_memory_map(boot_services);
+
+    // get Directory
+    let loaded_image = boot_services.handle_protocol::<LoadedImage>(_image).unwrap().get();
+    let device = unsafe{(*loaded_image).device()};
+    let file_system = boot_services.handle_protocol::<SimpleFileSystem>(device).unwrap().get();
+    let mut root_dir = unsafe{(*file_system).open_volume().unwrap()};
+
+    // let res = root_dir.read_entry(&mut b).unwrap();
+    // log::info!("{:?}", file_handle);
+    // let res = root_dir.read_entry(&mut b).unwrap();
+    // log::info!("{:?}", res);
+    // let res = root_dir.read_entry(&mut b).unwrap();
+    // log::info!("{:?}", res);
+
+    // get RegularFile 
+    let mut cstr_buf = [0u16; 32];
+    let cstr_file_name = CStr16::from_str_with_buf("kernel.elf", &mut cstr_buf).unwrap();
+    let file_handle = root_dir.open(cstr_file_name, FileMode::Read, FileAttribute::empty()).unwrap();
+    let mut file = unsafe {RegularFile::new(file_handle)};
+
+    let mut b = [0_u8; 1024 * 4]; // 本当はサイズ見てから必要な分を確保するべき
+    file.read(&mut b);
+    log::info!("{:?}", b);
 
     boot_services.stall(10_000_000);
 
@@ -41,6 +69,7 @@ fn get_memory_map(boot_services: &BootServices) {
     let (_map_key, desc_itr) = boot_services.memory_map(&mut memmap_buf).unwrap();
     let descriptors = desc_itr.copied().collect::<Vec<_>>();
     descriptors.iter().for_each(|descriptor| {
-        log::info!("{:?}, {}, {}, {}", descriptor.ty, descriptor.phys_start, descriptor.virt_start, descriptor.page_count);
+        // log::info!("{:?}, {}, {}, {}", descriptor.ty, descriptor.phys_start, descriptor.virt_start, descriptor.page_count);
     })
 }
+
