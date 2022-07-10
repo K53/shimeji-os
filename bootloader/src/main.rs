5acca91606a40e66c49ed818274576334884d7e3
keigo <devenv.keigo@gmail.com>
2022-07-10 23:31:50 +0900

    feat: create kernel loader

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -7,6 +7,9 @@ extern crate alloc;
 
 
 use core::arch::asm;
+use core::mem;
+use alloc::slice;
+use alloc::string::String;
 use alloc::vec::Vec;
 use core::fmt::Write;
 use uefi::prelude::*;
@@ -14,7 +17,13 @@ use uefi::{CStr16, Handle};
 use uefi::table::runtime::ResetType;
 use uefi::proto::loaded_image::LoadedImage;
 use uefi::proto::media::fs::SimpleFileSystem;
-use uefi::proto::media::file::{File, RegularFile, Directory, FileMode, FileAttribute};
+use uefi::proto::media::file::{File, RegularFile, Directory, FileMode, FileAttribute, FileInfo};
+use uefi::table::boot::{AllocateType, MemoryType}; 
+use goblin::elf;
+
+
+const KERNEL_BASE_ADDR: usize = 0x100000;
+const EFI_PAGE_SIZE: usize = 0x1000;
 
 #[entry]
 fn efi_main(_image: Handle, mut system_table: SystemTable<Boot>) -> Status {
@@ -32,27 +41,101 @@ fn efi_main(_image: Handle, mut system_table: SystemTable<Boot>) -> Status {
     get_memory_map(boot_services);
 
     // get Directory
-    let loaded_image = boot_services.handle_protocol::<LoadedImage>(_image).unwrap().get();
+    let loaded_image = boot_services.handle_protocol::<LoadedImage>(_image).unwrap().get(); //handle_protocolは非推奨 後で変える
     let device = unsafe{(*loaded_image).device()};
     let file_system = boot_services.handle_protocol::<SimpleFileSystem>(device).unwrap().get();
     let mut root_dir = unsafe{(*file_system).open_volume().unwrap()};
 
-    // let res = root_dir.read_entry(&mut b).unwrap();
-    // log::info!("{:?}", file_handle);
-    // let res = root_dir.read_entry(&mut b).unwrap();
-    // log::info!("{:?}", res);
-    // let res = root_dir.read_entry(&mut b).unwrap();
-    // log::info!("{:?}", res);
+    // ELF binary(Executable, Library)
+    //  - PIE(Position Independent Executable)
 
     // get RegularFile 
     let mut cstr_buf = [0u16; 32];
     let cstr_file_name = CStr16::from_str_with_buf("kernel.elf", &mut cstr_buf).unwrap();
-    let file_handle = root_dir.open(cstr_file_name, FileMode::Read, FileAttribute::empty()).unwrap();
+    let file_handle = root_dir.open(cstr_file_name, FileMode::Read, FileAttribute::empty()).unwrap();//.into_type().unwrap();
+    // let mut file = if let FileType::Regular(file) = file_handle {
+    //         file
+    //     }else{
+    //         panic!("This is not a regular file");
+    //     };
     let mut file = unsafe {RegularFile::new(file_handle)};
 
-    let mut b = [0_u8; 1024 * 4]; // 本当はサイズ見てから必要な分を確保するべき
-    file.read(&mut b);
-    log::info!("{:?}", b);
+    let file_size = file.get_boxed_info::<FileInfo>().unwrap().file_size() as usize;
+
+    let mut buf = vec![0; file_size]; // 本当はサイズ見てから必要な分を確保するべき
+    // let res = root_dir.read_entry(&mut b).unwrap();
+    // log::info!("{:?}", res);
+    // let res = root_dir.read_entry(&mut b).unwrap();
+    // log::info!("{:?}", res);
+    // let res = root_dir.read_entry(&mut b).unwrap();
+    // log::info!("{:?}", res);
+
+    file.read(&mut buf);
+    // log::info!("{:?}", buf);
+
+
+
+    // &Vec -> &[u8] -> (先頭アドレス)
+
+    let elf = elf::Elf::parse(&buf).unwrap();
+
+    let mut dest_start = usize::MAX;
+    let mut dest_end = 0;
+    for ph in elf.program_headers.iter() {
+        log::info!("Program header: {} {} {} {}",elf::program_header::pt_to_str(ph.p_type),ph.p_offset,ph.p_vaddr,ph.p_memsz);
+
+        if ph.p_type != elf::program_header::PT_LOAD { // If program header was not PT_LOAD, boot loader no need to put the binary in memory
+            continue;
+        }
+        dest_start = dest_start.min(ph.p_vaddr as usize); // Calculate the first memory address of the PT_LOAD binary section over the all binary sections
+        dest_end = dest_end.max(ph.p_vaddr + ph.p_memsz); // Calculate the last memory address of the PT_LOAD binary section over the all binary sections
+    }
+
+    boot_services.allocate_pages(AllocateType::Address(dest_start), MemoryType::LOADER_DATA,(dest_end as usize - dest_start as usize + EFI_PAGE_SIZE - 1) / EFI_PAGE_SIZE).unwrap();
+    // file.read(unsafe { core::slice::from_raw_parts_mut(KERNEL_BASE_ADDR as *mut u8, 4096_usize) }).unwrap();
+    file.close();
+
+    for ph in elf.program_headers.iter() {
+        if ph.p_type != elf::program_header::PT_LOAD { // If program header was not PT_LOAD, boot loader no need to put the binary in memory
+            continue;
+        }
+        let dest = unsafe {
+            slice::from_raw_parts_mut(ph.p_vaddr as *mut u8, ph.p_memsz as usize)
+        };
+        dest[..(ph.p_filesz as usize)].copy_from_slice(&buf[(ph.p_offset as usize)..(ph.p_offset as usize + ph.p_filesz as usize)]);
+        dest[(ph.p_filesz as usize)..].fill(0);
+    }
+
+    let entry_point: extern "sysv64" fn() = unsafe {
+        mem::transmute(elf.entry as usize)
+    };
+    entry_point();
+    // &ph.p_vaddr
+
+    // int a[4] = [0,1,2,3]
+    // *a => 0 
+
+    // int p_vaddr = 1234
+    // *p_vaddr
+
+    //         ポインタ &p_vaddr 
+    //         ↓
+    // [..... [1234] .......]
+
+ 
+
+
+    // [1234] <- 0xa123
+
+    // int address = 0xDEADBEAF;
+    // char* address_cast = (char*)address;
+    // addrress_cast[0] = 0;
+    // for(int i = 0; i < LENGTH; i++){
+    //     (*(*char)(address + i)) = 0;
+    // }
+
+
+    
 
     boot_services.stall(10_000_000);
 
--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,6 +1,6 @@
 #![no_std]
 #![no_main]
-#![feature(start)]
+// #![feature(start)]
 
 use core::arch::asm;
 use core::panic::PanicInfo;
@@ -12,22 +12,22 @@ fn panic(_info: &PanicInfo) -> ! {
     }
 }
 
-#[no_mangle]
-fn show_white(i: u32) {
-    // 白色なので15
-    let a: u8 = 15;
-    // 生ポインタを使って、15を代入
-    let ptr = unsafe { &mut *(i as *mut u8) };
-    *ptr = a 
-}
+// #[no_mangle]
+// fn show_white(i: u32) {
+//     // 白色なので15
+//     let a: u8 = 15;
+//     // 生ポインタを使って、15を代入
+//     let ptr = unsafe { &mut *(i as *mut u8) };
+//     *ptr = a 
+// }
 
 // https://skoji.jp/blog/2021/04/mikan-laranja-os.html
 // https://os.phil-opp.com/freestanding-rust-binary/
 #[no_mangle]
-pub extern "sysv64" fn _start() -> ! {
-    for i in 0xa0000..0xaffff {
-        show_white(i);
-    }
+pub extern "sysv64" fn kernel_main() -> ! {
+    // for i in 0xa0000..0xaffff {
+    //     show_white(i);
+    // }
     unsafe {
         loop {asm!("hlt")}
     }
